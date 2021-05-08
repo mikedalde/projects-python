605a3e2f7580db453579a89662c69ff7df4de5f2
mikedalde <mikedalde@gmail.com>
2021-05-08 08:52:31 -0500

    Base Commit

---
+++ madlibs-generator/app.py
@@ -0,0 +1,8 @@
+# Variables
+ritualname = input("Enter a ritual name: ")
+bodypart = input("Enter a bodypart: ")
+verb = input("Enter a verb: ")
+noun = input("Enter an adjective: ")
+
+# Adlib
+print("I have a morning ritual that I need to share. I call it '" + ritualname + "'. First I crouch down in the shower in the classic 'terminator traveling through time' pose. With my eyes open I crouch there for a minute, visualizing either Arnold or the guy from the second movie (not the chick in the third one because that one sucked) and I start to hum the terminator theme. Then I slowly rise to a standing position and " + verb + " my " + bodypart + ". It helps me to proceed through my day as an emotionless, cyborg badass. The only problem is if the shower curtain sticks to my terminator " + bodypart + ". It ruins the fantasy.")
